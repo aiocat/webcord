3dd0a4fad4619d74822000268063a3b5a43fb23d
aiocat <aiocat@protonmail.com>
2022-02-05 01:59:18 +0300

    load style from file

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 document.onreadystatechange = () => {
-  if (document.readyState === "complete") {
-    chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
-      let style = document.createElement("style"),
-        head = document.head || document.getElementsByTagName("head")[0];
+    if (document.readyState === "complete") {
+        chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
+            let style = document.createElement("style"),
+                head = document.head || document.getElementsByTagName("head")[0];
 
-      style.id = "WEBCORD_STYLE";
-      style.appendChild(document.createTextNode(res["WEBCORD_CSS"]));
-      head.appendChild(style);
-    });
-  }
+            style.id = "WEBCORD_STYLE";
+            style.appendChild(document.createTextNode(res["WEBCORD_CSS"]));
+            head.appendChild(style);
+        });
+    }
 };
--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
 chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
-  document.getElementById("css-box").value = res["WEBCORD_CSS"];
+    document.getElementById("css-box").value = res["WEBCORD_CSS"];
 });
 
 document.getElementById("inject").onclick = async () => {
-  let textCss = document.getElementById("css-box").value;
-  chrome.storage.sync.set({ WEBCORD_CSS: textCss }, function () {});
+    let textCss = document.getElementById("css-box").value;
+    chrome.storage.sync.set({ "WEBCORD_CSS": textCss }, function () { });
+    alert("Reload the Page!")
+};
+
+document.getElementById("from-file").onclick = async () => {
+    let file;
+    let input = document.createElement("input");
+    input.type = "file";
+
+    input.onchange = (e) => {
+        file = e.target.files[0];
+
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+
+        reader.onload = (readerEvent) => {
+            chrome.storage.sync.set({ "WEBCORD_CSS": readerEvent.target.result }, function () { });
+            document.getElementById("css-box").value = readerEvent.target.result;
+            alert("Reload the Page!")
+        };
+    };
+
+    input.click();
 };
