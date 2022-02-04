71f9da11f045205ae227c9cb41eeab7ab1fecb2b
aiocat <aiocat@protonmail.com>
2022-02-05 01:34:15 +0300

    style change

--- content.js
+++ content.js
@@ -1,13 +1,12 @@
 document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-        chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
-            let style = document.createElement("style"),
-                head = document.head || document.getElementsByTagName("head")[0],
-                css = res["WEBCORD_CSS"];
+  if (document.readyState === "complete") {
+    chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
+      let style = document.createElement("style"),
+        head = document.head || document.getElementsByTagName("head")[0];
 
-            style.id = "WEBCORD_STYLE";
-            style.appendChild(document.createTextNode(css));
-            head.appendChild(style);
-        });
-    }
+      style.id = "WEBCORD_STYLE";
+      style.appendChild(document.createTextNode(res["WEBCORD_CSS"]));
+      head.appendChild(style);
+    });
+  }
 };
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
+chrome.storage.sync.get(["WEBCORD_CSS"], function (res) {
+  document.getElementById("css-box").value = res["WEBCORD_CSS"];
+});
+
 document.getElementById("inject").onclick = async () => {
-    let textCss = document.getElementById("css-box").value;
-    chrome.storage.sync.set({ "WEBCORD_CSS": textCss }, function () { });
+  let textCss = document.getElementById("css-box").value;
+  chrome.storage.sync.set({ WEBCORD_CSS: textCss }, function () {});
 };
